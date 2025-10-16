23be4d8c9b5616d2d4017feec4ce36195c126e81
matiasvlevi <matiasvlevi@gmail.com>
2025-10-16 18:42:05 -0400

    different node paths for dev/prod

--- src/api/home/controllers/home.ts
+++ src/api/home/controllers/home.ts
@@ -8,11 +8,13 @@ import path from 'path';
 export default factories.createCoreController('api::home.home', ({ }) => ({
   async buildSite(ctx) {
     const sitePath = '../site';
-    const nodePath = '/home/vlev/.nvm/versions/node/v20.17.0/bin/node';
+    const nodePath = process.env.API_HOST.includes(`https`) ? 
+      '/root/.nvm/versions/node/v22.20.0/bin/node' : 
+      '/home/vlev/.nvm/versions/node/v20.17.0/bin/node';
 
     const result = await new Promise<string>((resolve) => {
       exec(
-        `cd ${sitePath} && ${nodePath} ./node_modules/.bin/next build`,
+        `cd ${sitePath} && rm -fr .next && ${nodePath} ./node_modules/.bin/next build`,
         { cwd: sitePath, env: { ...process.env, NODE_ENV: 'production', HOST:process.env.API_HOST } },
         (error, stdout, stderr) => {
           if (error) {
