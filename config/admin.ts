f07da944db5256e1525c261ac6795452fe9bc9b0
matiasvlevi <matiasvlevi@gmail.com>
2026-02-23 11:04:04 -0500

    add preview, article and author

--- config/admin.ts
+++ config/admin.ts
@@ -1,5 +1,22 @@
 export default ({ env }) => ({
   serveAdminPanel: env.bool('SERVE_ADMIN', true),
+  preview: {
+    enabled: true,
+    config: {
+      allowedOrigins: env("CLIENT_URL"),
+      async handler(uid, { documentId, locale, status }) {
+        const document = await strapi.documents(uid).findOne({ documentId, locale });
+
+        switch(uid) {
+          case 'api::article.article': return `${env('CLIENT_URL')}/fr/blog/${document.slug}`
+          case 'api::page.page': 
+          default:  
+            return `${env('CLIENT_URL')}/${document.locale || locale || 'en'}/${document.slug}`
+        }
+
+      },
+    }
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
